b1ddee9430b87beb74e6782707ba161bb8275e20
Kahnu Charan Swain <djkanha044@gmail.com>
2023-04-18 16:10:20 +0530

    Only Support ACF Version Less Than 6.0.7

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # acf-repeater-field
+
+Only supports ACF Version less than 6.0.7
--- acf-repeater.php
+++ acf-repeater.php
@@ -9,17 +9,18 @@ Author URI: https://github.com/kahnu044/
 Copyright: kahnu044
 */
 
-if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-if( !class_exists('acf_plugin_repeater') ):
+if (!class_exists('acf_plugin_repeater')) :
 
-class acf_plugin_repeater {
+	class acf_plugin_repeater
+	{
 
-	// vars
-	var $settings;
+		// vars
+		var $settings;
 
 
-	/*
+		/*
 	*  __construct
 	*
 	*  This function will setup the class functionality
@@ -32,38 +33,38 @@ class acf_plugin_repeater {
 	*  @return	n/a
 	*/
 
-	function __construct() {
+		function __construct()
+		{
 
-		// vars
-		$this->settings = array(
+			// vars
+			$this->settings = array(
 
-			// basic
-			'name'				=> __('Advanced Custom Fields: Repeater Field', 'acf'),
-			'version'			=> '2.1.0',
+				// basic
+				'name'				=> __('Advanced Custom Fields: Repeater Field', 'acf'),
+				'version'			=> '2.1.0',
 
-			// urls
-			'slug'				=> dirname(plugin_basename( __FILE__ )),
-			'basename'			=> plugin_basename( __FILE__ ),
-			'path'				=> plugin_dir_path( __FILE__ ),
-			'dir'				=> plugin_dir_url( __FILE__ ),
+				// urls
+				'slug'				=> dirname(plugin_basename(__FILE__)),
+				'basename'			=> plugin_basename(__FILE__),
+				'path'				=> plugin_dir_path(__FILE__),
+				'dir'				=> plugin_dir_url(__FILE__),
 
-		);
+			);
 
-		// include v5 field
-		add_action('acf/include_field_types', array($this, 'include_field_types'));
+			// include v5 field
+			add_action('acf/include_field_types', array($this, 'include_field_types'));
 
-		// include v4 field
-		add_action('acf/register_fields', array($this, 'include_field_types'));
+			// include v4 field
+			add_action('acf/register_fields', array($this, 'include_field_types'));
 
-		// include updates
-		if( is_admin() ) {
-			$this->include_file('acf-repeater-update.php');
+			// include updates
+			if (is_admin()) {
+				$this->include_file('acf-repeater-update.php');
+			}
 		}
 
-	}
 
-
-	/*
+		/*
 	*  include_file
 	*
 	*  This function will check if a file exists before including it
@@ -76,13 +77,14 @@ class acf_plugin_repeater {
 	*  @return	n/a
 	*/
 
-	function include_file( $file = '' ) {
-		$file = dirname(__FILE__) . '/'. $file;
-		if( file_exists($file) ) include_once( $file );
-	}
+		function include_file($file = '')
+		{
+			$file = dirname(__FILE__) . '/' . $file;
+			if (file_exists($file)) include_once($file);
+		}
 
 
-	/*
+		/*
 	*  include_field_types
 	*
 	*  This function will include the v5 field type
@@ -95,41 +97,41 @@ class acf_plugin_repeater {
 	*  @return	n/a
 	*/
 
-	function include_field_types() {
+		function include_field_types()
+		{
 
-		// vars
-		$version = '';
+			// vars
+			$version = '';
 
-		// // version 5
-		// if( defined('ACF_VERSION') ) {
+			// version 5
+			if (defined('ACF_VERSION')) {
 
-		// 	// version 5.7+
-		// 	if( version_compare(ACF_VERSION, '5.7.0', '>=') ) {
-		// 		$version = '5-7';
+				// version 5.7+
+				if (version_compare(ACF_VERSION, '5.7.0', '>=')) {
+					$version = '5-7';
 
-		// 	//  version 5.0
-		// 	} else {
-		// 		$version = '5-0';
-		// 	}
+					//  version 5.0
+				} else {
+					$version = '5-0';
+				}
 
-		// // version 4
-		// } else {
-		// 	$version = '4-0';
-		// }
-		$version = '5-7';
+				// version 4
+			} else {
+				$version = '4-0';
+			}
 
-		// include
-		$this->include_file( "includes/$version/acf-repeater-field.php" );
+			// include
+			$this->include_file("includes/$version/acf-repeater-field.php");
+		}
 	}
-}
 
 
-// globals
-global $acf_plugin_repeater;
+	// globals
+	global $acf_plugin_repeater;
 
 
-// instantiate
-$acf_plugin_repeater = new acf_plugin_repeater();
+	// instantiate
+	$acf_plugin_repeater = new acf_plugin_repeater();
 
 
 // end class
